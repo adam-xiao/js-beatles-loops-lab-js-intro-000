b0cb7de989cd22516e903e1860a9f720416324d0
Adam Xiao <adam.x.xiao@gmail.com>
2019-08-08 14:02:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,16 +20,17 @@ function johnLennonFacts(facts){
   return newFacts;
 }
 
+var i = 0;
+function incVar(){
+  i = i + 1;
+  return i;
+}
+
 function iLoveTheBeatles(number){
   var love = [];
-  var i = 0;
-  function incVar(){
-    i = i + 1;
-    return i;
-  }
   do{
     love.push("I love the Beatles!");
   while (incVar() < 15);
-  return love;
   }
+  return love;
 }
